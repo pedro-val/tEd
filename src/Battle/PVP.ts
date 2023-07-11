fb189812ae8456cd0e00a852a368a38ea6d6a957
devpedroval <prmval@gmail.com>
2023-07-11 15:33:04 -0300

    requisito 10 completo

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,18 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+export default class PVP extends Battle {
+  constructor(protected player1: Fighter, protected player2: Fighter) {
+    super(player1);
+  }
+    
+  fight(): number {
+    const { player1 } = this;
+    const { player2 } = this;
+    while (player1.lifePoints > 0 && player2.lifePoints > 0) {
+      player1.attack(player2);
+      player2.attack(player1);
+    }
+    return super.fight();
+  }
+}
